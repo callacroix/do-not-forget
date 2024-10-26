e14c1746d3357481a93088bf3e03c1482e977581
Carine Lacroix <callacroix@gmail.com>
2024-10-26 16:19:15 +0200

    set up basic classes to start

---
+++ app/src/main/java/com/wedding/thinking/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.wedding.thinking
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.wedding.thinking.ui.theme.ThinkingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ThinkingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
